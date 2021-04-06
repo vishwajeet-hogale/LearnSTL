495258a045ff936a635ea445e6a7800cb5de2553
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-04-06 18:16:13 +0530

    knapsack iterative added

---
+++ Problems/knapsack.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+int cmp(pair<int,float>a,pair<int,float>b){
+    return a.first>=b.first;
+}
+int knapsack(int capacity,vector<float> profit,vector<float> weight,int n){
+    float max = 0;
+    vector<pair<int,float>> proweight;
+ 
+    for(int j=0;j<n;j++){
+
+     
+        proweight.push_back(make_pair(j,float(profit[j]/weight[j])));
+
+    }
+    sort(proweight.begin(),proweight.end(),cmp);
+    int i=0;
+    for(auto &x:proweight){
+
+        if(capacity > 0 and weight[x.first]<=capacity){
+            capacity = capacity - weight[x.first];
+            max = max + profit[x.first];
+        }
+        else{
+            break;
+        }
+        if(capacity>0){
+            auto nx = next(&x,1);
+            // cout<<capacity / weight[nx->first]<<endl;
+            max = max + float(profit[nx->first] * (capacity / weight[nx->first])); 
+            capacity = 0;
+        }
+        // cout<<max<<endl;
+    }
+    return max;
+}
+int main(){
+    cout<<"Enter the number of elements:"<<endl;
+    int n;
+    cin>>n;
+    vector<float> profit;
+    vector<float> weight;
+    int capacity;
+    cout<<"Enter the capacity of the bag in terms of weight:"<<endl;
+    cin>> capacity;
+    int temp,temp1;
+    for(int i=0;i<n;i++){
+        cout<<"Enter the profit and weight for article number : "<<i<<endl;
+        cin>>temp>>temp1;
+        profit.push_back(temp);
+        weight.push_back(temp1);
+    }
+    cout<<"The maximum profit will be: " << knapsack(capacity,profit,weight,n)<<endl;
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 ## Questions
     1) Split the array into two partions such that the sum of both partitons is the same. 
         Constraint : You can swap one element from one partition to another.
-    2)On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
+    2) On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
 
     You can move according to these rules:
 
@@ -14,6 +14,8 @@
     move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
     You have to visit the points in the same order as they appear in the array.
     You are allowed to pass through points that appear later in the order, but these do not count as visits.
+    3) Iterative knapsack problem solution.
 ## Answers  File name according to numbers 
     1) equalarr.cpp (c++ STL approach)
     2) min-path-2d-plane.cpp
+    3) knapsack.cpp
