3028e05fecdf7a1844ebdf3f6514b26fe01efe58
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-04-22 13:19:55 +0530

    Next Permutation

--- Problems/nextpermutation.cpp
+++ Problems/nextpermutation.cpp
@@ -9,61 +9,68 @@ Step 4: Reverse A[k+1] to the end.
 
 Time complexity = O(n) and space complexity = o(1)
 */
-#include<iostream>
-#include<bits/stdc++.h>
-#include<algorithm>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
 
-vector<int> next_permutation(vector<int> num){
-    
+vector<int> next_permutation(vector<int> num)
+{
 
     int sz = num.size();
-        int k=-1;
-        int l;
-        //step1
-        for (int i=0;i<sz-1;i++){
-            if (num[i]<num[i+1]){
-                k=i;
-            }
+    int k = -1;
+    int l;
+    //step1
+    for (int i = 0; i < sz - 1; i++)
+    {
+        if (num[i] < num[i + 1])
+        {
+            k = i;
         }
-        if (k==-1){
-            sort(num.begin(),num.end());
-            return num;
-        }
-        
-        //step2
-        for (int i=k+1;i<sz;i++){
-            if (num[i]>num[k]){
-                l=i;
-            }
-        }
-        //step3        
-        int tmp = num[l];
-        num[l]=num[k];
-        num[k]=tmp;
-        //step4
-        reverse(num.begin()+k+1,num.end());
+    }
+    if (k == -1)
+    {
+        sort(num.begin(), num.end());
         return num;
     }
 
-int main(){
+    //step2
+    for (int i = k + 1; i < sz; i++)
+    {
+        if (num[i] > num[k])
+        {
+            l = i;
+        }
+    }
+    //step3
+    int tmp = num[l];
+    num[l] = num[k];
+    num[k] = tmp;
+    //step4
+    reverse(num.begin() + k + 1, num.end());
+    return num;
+}
+
+int main()
+{
     vector<int> m;
     m.push_back(6);
     m.push_back(2);
-    
+
     m.push_back(1);
-    
+
     m.push_back(5);
-    
+
     m.push_back(4);
-    
+
     m.push_back(3);
-    
+
     m.push_back(0);
     vector<int> res = next_permutation(m);
-    for(int &i:res){
-        cout<< i<<",";
+    for (int &i : res)
+    {
+        cout << i << ",";
     }
-    cout<<"\n";
+    cout << endl;
     return 0;
 }
