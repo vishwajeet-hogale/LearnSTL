05b1046aee32e28917dc9db6a7adf4ef06b4b686
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-06-06 00:38:20 +0530

    Number of paths fro mstart to end Recursive solution

--- Problems/Dynamic_Programming/NumberOfWaysStartToEnd.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-vector<vector<int>> grid({
-    {1,1,1},
-    {1,0,1},
-    {1,1,1}
-});
---
+++ Problems/Dynamic_Programming/numWays.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> grid({
+    {1,1,1},
+    {1,0,1},
+    {1,1,1}
+});
+// Recursive solution 
+bool valid(int i,int j){
+    return i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && grid[i][j] == 1 ;
+}
+int numberOfWays(int i,int j){
+    if(!valid(i,j)){
+        return 0;
+    }
+    if(i == grid.size()-1 && j == grid[0].size()-1){
+        return 1;
+    }
+    return numberOfWays(i+1,j) + numberOfWays(i,j+1);
+}
+// Memoization involved : Overlapping subsets and optimal substructure property 
+// is satisfied and hence DP should be used
+
+// int numberOfWays(int i,int j){
+//     if(!valid(i,j)){
+//         return 0;
+//     }
+//     if(i == grid.size()-1 && j = grid[0].size()-1){
+//         return 1;
+//     }
+//     return numberOfWays(i=1,j) + numberOfWays(i,j=1);
+// }
+int main(){
+    cout<<numberOfWays(0,0)<<endl;
+    return 0;
+}
