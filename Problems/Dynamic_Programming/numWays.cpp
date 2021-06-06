c719a45a15d6716218825c9ac441f15773778488
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-06-06 11:54:05 +0530

    Number of ways from start to end memoized solution

--- Problems/Dynamic_Programming/numWays.cpp
+++ Problems/Dynamic_Programming/numWays.cpp
@@ -10,27 +10,32 @@ vector<vector<int>> grid({
 bool valid(int i,int j){
     return i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && grid[i][j] == 1 ;
 }
-int numberOfWays(int i,int j){
-    if(!valid(i,j)){
-        return 0;
-    }
-    if(i == grid.size()-1 && j == grid[0].size()-1){
-        return 1;
-    }
-    return numberOfWays(i+1,j) + numberOfWays(i,j+1);
-}
-// Memoization involved : Overlapping subsets and optimal substructure property 
-// is satisfied and hence DP should be used
-
 // int numberOfWays(int i,int j){
 //     if(!valid(i,j)){
 //         return 0;
 //     }
-//     if(i == grid.size()-1 && j = grid[0].size()-1){
+//     if(i == grid.size()-1 && j == grid[0].size()-1){
 //         return 1;
 //     }
-//     return numberOfWays(i=1,j) + numberOfWays(i,j=1);
+//     return numberOfWays(i+1,j) + numberOfWays(i,j+1);
 // }
+// Memoization involved : Overlapping subsets and optimal substructure property 
+// is satisfied and hence DP should be used
+vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+int numberOfWays(int i,int j){
+    if(!valid(i,j)){
+        return 0;
+    }
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+    if(i == grid.size()-1 && j == grid[0].size()-1){
+        return 1;
+    }
+    dp[i][j] =  numberOfWays(i+1,j) + numberOfWays(i,j+1);
+    return dp[i][j];
+}
+
 int main(){
     cout<<numberOfWays(0,0)<<endl;
     return 0;
